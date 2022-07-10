3434e78cc14b6f04397cd8390fa9af57a653ddf2
alanwindesheim <55313885+alanwindesheim@users.noreply.github.com>
2022-07-10 21:46:25 +0200

    k

--- components/HomepageHero/HomepageHero.js
+++ components/HomepageHero/HomepageHero.js
@@ -23,7 +23,7 @@ const HomepageHero = () => {
         sources: [
           {
             width: "500",
-            src: "https://stream.mux.com/GJjLF93MGEmq4VfidIdZ4oMMAJRhEjSQ.m3u8",
+            src: "http://eurokurd.mymediapc.net:40/live/Eurokurd/playlist.m3u8",
             type: "application/x-mpegURL",
           },
         ],
